4bc8a8ba00e1abf52392426c800c2f495cfe4350
Rudi <flaing12@gmail.com>
2015-04-17 01:41:04 +0800

    ++

---
+++ profiles.clj
@@ -0,0 +1,6 @@
+{:user {
+  :java-cmd "D:\\Programs\\Java\\jdk1.8.0_05\\bin\\java.exe"
+  :plugins [[cider/cider-nrepl "0.8.1"]
+            [lein-pprint "1.1.1"]
+			[org.clojure/data.csv "0.1.2"]]
+           }}
